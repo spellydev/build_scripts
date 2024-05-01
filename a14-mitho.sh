670739b7844cb301204dbf615fc739007f66e34f
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-05-02 01:35:59 +0530

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,8 +1,8 @@
-rm -rf .repo/local_manifests/  && \
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs && \
-git clone https://github.com/PhantomEnigma/local_manifests -b a14 .repo/local_manifests && \
-/opt/crave/resync.sh  && \
-. build/envsetup.sh && \
-lunch lineage_Mi439_4_19-ap1a-userdebug && \
-make installclean && \
-mka bacon
+rm -rf .repo/local_manifests/
+repo init -u https://github.com/Evolution-X/manifest -b u
+git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+/opt/crave/resync.sh
+. build/envsetup.sh
+lunch evolution_mi439-ap1a-userdebug
+make installclean
+m evolution
