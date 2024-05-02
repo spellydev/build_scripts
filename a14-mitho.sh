e33470a9710268eb32d9d4cb8d9a84dacbb35b70
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-05-02 10:23:44 +0530

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,4 +1,4 @@
-rm -rf .repo/local_manifests/
+rm -rf .repo
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
 /opt/crave/resync.sh
