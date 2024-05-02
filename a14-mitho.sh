7c5ff0054f2d7eb4e312c9c78af7ba3fe4dacaa2
PhantomEnigma <1212ravi@proton.me>
2024-05-02 11:44:34 +0530

    created new branch for rising mithorium base

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests/
 #repo init rom
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 #Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+git clone https://github.com/PhantomEnigma/local_manifests -b a14 .repo/local_manifests
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
