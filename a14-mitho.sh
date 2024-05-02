1ea0cf5a6e6d15e7346d38c2804bd0c25b9a6108
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-05-02 08:30:16 +0530

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,8 +1,8 @@
 rm -rf .repo/local_manifests/
-repo init -u https://github.com/Evolution-X/manifest -b u
-git clone https://github.com/PhantomEnigma/local_manifests -b a14 .repo/local_manifests
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
 /opt/crave/resync.sh
 . build/envsetup.sh
-lunch evolution_Mi439_4_19-ap1a-userdebug
+riseup Mi439_4_19 userdebug 
 make installclean
-m evolution
+rise b
