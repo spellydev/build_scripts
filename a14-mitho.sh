1af748e888d4bc447fff640cf644f6b393c70e56
PhantomEnigma <1212ravi@proton.me>
2024-05-02 11:28:53 +0530

    added comments

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,6 +1,9 @@
 rm -rf .repo/local_manifests/
+#repo init rom
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+#Local manifests
 git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+#build
 /opt/crave/resync.sh
 . build/envsetup.sh
 riseup Mi439_4_19 userdebug 
