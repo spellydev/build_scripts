817af32aeb3af333e927f022178d7c578e5d21ea
PhantomEnigma <1212ravi@proton.me>
2024-05-03 08:50:32 +0530

    nothing

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -3,6 +3,7 @@ rm -rf .repo/local_manifests/
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 #Local manifests
 git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
