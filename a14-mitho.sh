6f13b34319ff0bb0e8561dd9be6aff92e0a24e44
PhantomEnigma <1212ravi@proton.me>
2024-05-03 12:14:37 +0530

    build continue

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,13 +1,14 @@
-rm -rf .repo/local_manifests/
+#rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+#repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 #Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests -b a14 .repo/local_manifests
+#git clone https://github.com/PhantomEnigma/local_manifests -b a14 .repo/local_manifests
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
 riseup Mi439_4_19 userdebug 
-make installclean
+#make installclean
 cd kernel/xiaomi/mithorium-4.19/kernel
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+croot
 rise b
