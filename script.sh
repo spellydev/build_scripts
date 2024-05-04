4c60dc5c679d88b37ccfe58b5fdffeb30d89f1a1
PhantomEnigma <1212ravi@proton.me>
2024-05-04 11:50:18 +0530

    new branch for evo mclo

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b u
 #Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2-evo .repo/local_manifests
 
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
-brunch mi439 || brunch lineage_mi439-ap1a-userdebug
+lunch evolution_mi439-ap1a-userdebug
+make installclean
+m evolution
