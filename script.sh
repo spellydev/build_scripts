d18d2c877b91afb6ab69f332cb73c4ff3d1255c0
PhantomEnigma <1212ravi@proton.me>
2024-05-04 06:58:25 +0530

    new branch for matrixx mclo

--- script.sh
+++ script.sh
@@ -1,15 +1,10 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 #Local manifests
 git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
 
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
-riseup mi439 userdebug
-make installclean
-cd kernel/xiaomi/sdm439
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
-croot
-rise b
+brunch mi439 || brunch lineage_mi439-ap1a-userdebug
