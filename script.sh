749bcffbb15f27d006b42de04a58a975dfc0a6d7
PhantomEnigma <1212ravi@proton.me>
2024-05-05 08:54:32 +0530

    Crdroid script

--- script.sh
+++ script.sh
@@ -1,14 +1,16 @@
 rm -rf .repo/local_manifests/
-repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-Local manifests
+#repo init rom
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+#Local manifests
 git clone https://github.com/PhantomEnigma/local_manifests .repo/local_manifests
-build
+#build
 /opt/crave/resync.sh
 . build/envsetup.sh
-riseup Mi439_4_19 userdebug 
+lunch lineage_Mi439_4_19-ap1a-userdebug
 make installclean
+#KSU
 cd kernel/xiaomi/mithorium-4.19/kernel
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
 croot
-rise b
+brunch lineage_Mi439_4_19-ap1a-userdebug || brunch Mi439_4_19
+
