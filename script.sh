3adb2a20b36870376724634e77aee7745399ba10
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-05-06 22:34:40 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
-rm -rf .repo/local_manifests/
+#rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14
+#repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14
 #Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2-derp .repo/local_manifests
+#git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2-derp .repo/local_manifests
 
 #build
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 . build/envsetup.sh
 lunch derp_mi439-ap1a-userdebug || lunch derp_mi439-userdebug
-make installclean
+#make installclean
 mka derp
