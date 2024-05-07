2acc7f6b7c7c211713d56e87a6e3c57d789b0a4e
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-05-07 06:52:44 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/Evolution-X/manifest -b u
+repo init -u https://github.com/Evolution-XYZ/manifest -b udc --git-lfs
 #Local manifests
 git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2-evo .repo/local_manifests
 
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
-lunch evolution_mi439-userdebug
+lunch lineage_mi439-ap1a-userdebug || lunch lineage_mi439-userdebug
 make installclean
 m evolution
