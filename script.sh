23f9eba97d94d5bbc7ad516f7236fc1484d0756b
PhantomEnigma <1212ravi@proton.me>
2024-05-08 18:38:29 +0530

    matx

--- script.sh
+++ script.sh
@@ -2,8 +2,10 @@ rm -rf .repo/local_manifests/
 #repo init rom
 repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 #Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests -b a14-evo .repo/local_manifests
+git clone https://github.com/PhantomEnigma/local_manifests -b a14-matx .repo/local_manifests
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
+lunch lineage_Mi439-ap1a-userdebug
+make installclean
 brunch Mi439_4_19 || brunch lineage_Mi439_4_19-ap1a-userdebug || brunch lineage_Mi439_4_19-userdebug
