c372448b150d204ba892b6c6da01b1cb22128836
PhantomEnigma <1212ravi@proton.me>
2024-05-08 18:01:33 +0530

    some tiny fixes

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests/
 #repo init rom
 repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
 #Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests .repo/local_manifests
+git clone https://github.com/PhantomEnigma/local_manifests .repo/local_manifests -b a14
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
@@ -12,5 +12,5 @@ make installclean
 cd kernel/xiaomi/mithorium-4.19/kernel
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
 croot
-brunch lineage_Mi439_4_19-ap1a-userdebug || brunch Mi439_4_19
+brunch Mi439_4_19 || brunch lineage_Mi439_4_19-ap1a-userdebug
 
