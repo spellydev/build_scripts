45db80f7e5441116be9a7090c272b30361df11bf
PhantomEnigma <1212ravi@proton.me>
2024-05-17 21:52:49 +0530

    blaze

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14-QPR2
 #Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests -b a14-matx .repo/local_manifests
+git clone https://github.com/PhantomEnigma/local_manifests -b a14-blaze .repo/local_manifests
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
-lunch lineage_Mi439-ap1a-userdebug
+lunch blaze_mi439-userdebug || lunch blaze_mi439-ap1a-userdebug
 make installclean
-brunch Mi439_4_19 || brunch lineage_Mi439_4_19-ap1a-userdebug || brunch lineage_Mi439_4_19-userdebug
+make bacon
