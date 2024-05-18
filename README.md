4fa6ed38ab5b98047edd898249515dcccc43d6b9
PhantomEnigma <1212ravi@proton.me>
2024-05-18 08:14:15 +0530

    bliss

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # build_scripts
 
 ```
-curl https://raw.githubusercontent.com/PhantomEnigma/build_scripts/blaze-14-mclo/script.sh | bash
+curl https://raw.githubusercontent.com/PhantomEnigma/build_scripts/bliss-14-mclo/script.sh | bash
 ```
 
 ### For crave
 ```
-crave run --clean --no-patch -- "curl https://raw.githubusercontent.com/PhantomEnigma/build_scripts/blaze-14-mclo/script.sh | bash"
+crave run --clean --no-patch -- "curl https://raw.githubusercontent.com/PhantomEnigma/build_scripts/bliss-14-mclo/script.sh | bash"
 ```
--- script.sh
+++ script.sh
@@ -1,11 +1,9 @@
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14-QPR2
+repo init -u https://github.com/BlissRoms/platform_manifest.git -b universe --git-lfs
 #Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2-blaze .repo/local_manifests
+git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2-bliss .repo/local_manifests
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
-lunch blaze_mi439-userdebug || lunch blaze_mi439-ap1a-userdebug
-make installclean
-make bacon
+blissify -c -v mi439 || blissify -c -v bliss_mi439
