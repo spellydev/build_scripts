b14f44bfb15fb0b69e16331fd3ab2c55f507074d
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-06-01 16:57:12 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,25 @@
 rm -rf .repo/local_manifests/
+
 # repo init rom
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14
+
 # Local manifests
 git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2-derp .repo/local_manifests
 
 # build
 /opt/crave/resync.sh
-. build/envsetup.sh
+
+# Set up build environment
+source build/envsetup.sh
+
+# Setup Git LFS
+repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
+
+# Lunch
 lunch derp_mi439-ap1a-userdebug || lunch derp_mi439-userdebug
+
+# Make cleaninstall
 make installclean
+
+# Build rom
 mka derp
