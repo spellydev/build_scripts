765c2317699c3126ca6baa171a63043985994c66
Phantom <166353747+PhantomEnigma@users.noreply.github.com>
2024-06-07 18:04:12 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,28 +5,38 @@ repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14
 echo "=================="
 echo "Repo init success"
 echo "=================="
+
 # Local manifests
 git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2-derp .repo/local_manifests
+echo "=================="
+echo "Local manifest clone success"
+echo "=================="
 
 # build
 /opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
+
 # keys
 git clone https://github.com/PhantomEnigma/build_keys.git -b keys build_keys
 cp build_keys/* vendor/derp/signing/keys
 echo "============="
 echo "Keys copied"
 echo "============="
-# pico
-rm -rf vendor/gms/gms_pico.mk
+
+# pico gapps
 git clone https://github.com/PhantomEnigma/scripts.git -b pico gms_pico
 cp gms_pico/gms_pico.mk vendor/gms
+echo "====================="
+echo "useless gapps removed"
+echo "====================="
+
 # Export
 export BUILD_USERNAME=Phantom
 export BUILD_HOSTNAME=crave
 echo "============="
+
 # Set up build environment
 source build/envsetup.sh
 echo "============="
