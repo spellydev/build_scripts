fa672153583564d337cdbbc3c30b014834dff288
PhantomEnigma <1212ravi@proton.me>
2024-06-13 19:18:29 +0530

    initial

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b QPR2 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Phantm7/local_manifests_clo -b udc-2-superior .repo/local_manifests
+git clone https://github.com/Phantm7/local_manifests_clo -b udc-2-infinity .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -23,14 +23,14 @@ echo "============="
 # Export
 export BUILD_USERNAME=Phantom
 export BUILD_HOSTNAME=crave
-echo "============="
+echo "======= Export Done ======"
 
 # Set up build environment
 source build/envsetup.sh
-echo "============="
-# Setup Git LFS
-# repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
+echo "====== Envsetup Done ======="
 
 # Lunch
-breakfast mi439
-m bacon
+lunch infinity_mi439-userdebug || lunch infinity_mi439-ap1a-userdebug
+echo "====== lunch success ======"
+
+mka bacon
