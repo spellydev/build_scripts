be6301339a740d0e921a6313eaa5fbb3a9c2b76a
PhantomEnigma <1212ravi@proton.me>
2024-06-13 20:34:35 +0530

    Rising

--- script.sh
+++ script.sh
@@ -1,12 +1,34 @@
-rm -rf .repo/local_manifests/
-#repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-#Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+#!/bin/bash
 
-#build
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/Phantm7/local_manifests_clo -b udc-2-rising .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# build
 /opt/crave/resync.sh
-. build/envsetup.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=Phantom
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
 riseup mi439 userdebug
-make installclean
-rise b
+rise sb
