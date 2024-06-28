4ed3347d8910b59f2a9784593e19ec500a4649a4
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-06-28 08:40:02 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,12 @@
-rm -rf .repo/local_manifests/
-
-#repo init rom
-repo init --depth=1 -u https://github.com/PixelOS-Pixelish/manifest -b eleven
-
-#Local manifests
-git clone https://github.com/Gtajisan/local_manifests_clo -b Udc-2-pixelOS .repo/local_manifests
-
-#build
-/opt/crave/resync.sh
-. build/envsetup.sh
-lunch aosp_mi439-userdebug || lunch aosp_mi439-ap1a-userdebug
-mka bacon -jX
-mka bacon
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
+git clone https://github.com/Gtajisan/local_manifests_clo.git --depth 1 -b Udc-2-pixelOS .repo/local_manifests && \
+repo init -u https://github.com/PixelOS-Pixelish/manifest -b eleven; \
+/opt/crave/resync.sh && \
+source build/envsetup.sh && \
+lunch aosp_mi439-userdebug || lunch aosp_mi439-ap1a-userdebug && \
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true; \
+export BUILD_USERNAME=Gtajisan; \
+export BUILD_HOSTNAME=Gtajisan@crave.io; \
+export KBUILD_BUILD_USER=Gtajisan; \
+export KBUILD_BUILD_HOST=Gtajisan@crave.io; \
+mka bacon -jX"
