ea761ca068e68ce97f19f488f13d01ae984ee04e
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-06-29 14:04:29 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,15 @@
+#ProjectSakura building scrpit for crave
+
 rm -rf .repo/local_manifests/
-#repo init rom
-repo init -u https://github.com/Evolution-XYZ/manifest -b udc --git-lfs
+
+#Repo init rom
+repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
+
 #Local manifests
-git clone https://github.com/Gtajisan/local_manifests_clo -b udc-2 .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b udc-2-derp .repo/local_manifests
 
 #build
 /opt/crave/resync.sh
 . build/envsetup.sh
-lunch evolution_mi439-ap1a-userdebug || lunch evolution_mi439-userdebug
-make installclean
-m evolution
+lunch lineage_mi439-ap1a-userdebug
+mka baco
