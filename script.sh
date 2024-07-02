d65581b2267cfd4573c86b639d8f1791e23b91ff
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-02 14:12:00 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,9 +10,9 @@ echo "=================="
 
 # Local manifests
 git clone https://github.com/Gtajisan/local_manifests_clo -b udc-2-rising .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+echo "================================================="
+echo "hey frn your fucking Local manifest clone success"
+echo "================================================="
 
 # build
 /opt/crave/resync.sh
@@ -23,14 +23,14 @@ echo "============="
 echo "===== Cherry-pick stuff started ====="
 cd packages/apps/Updater
 git fetch rising --unshallow
-git fetch https://github.com/Gtajisan/android_packages_apps_Updater fourteen
+git fetch https://github.com/Phantm7/android_packages_apps_Updater fourteen
 git cherry-pick 022c468
 cd ../../..
 echo "===== Cherry-pick Ended ====="
 
 # keys
 rm -rf vendor/lineage-priv
-git clone https://github.com/Gtajisan/build_keys.git -b lin-keys vendor/lineage-priv
+git clone https://github.com/Phantm7/build_keys.git -b lin-keys vendor/lineage-priv
 echo "============="
 echo "Keys copied"
 echo "============="
@@ -46,7 +46,7 @@ done
 echo "===== Remove overrides Success ====="
 
 # Export
-export BUILD_USERNAME=Gtajisan
+export BUILD_USERNAME=Phantom
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
