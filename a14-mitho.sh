9d78d0467a2dfef3671c2b7d26bd8824df39baf7
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-02 13:56:45 +0600

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,14 +1,59 @@
+#!/bin/bash
+
 rm -rf .repo/local_manifests/
-repo init rom
+
+# repo init rom
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests .repo/local_manifests
-build
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/Phantm7/local_manifests_clo -b udc-2-rising .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# build
 /opt/crave/resync.sh
-. build/envsetup.sh
-riseup Mi439_4_19 userdebug 
-make installclean
-cd kernel/xiaomi/mithorium-4.19/kernel
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
-croot
+echo "============="
+echo "Sync success"
+echo "============="
+
+echo "===== Cherry-pick stuff started ====="
+cd packages/apps/Updater
+git fetch rising --unshallow
+git fetch https://github.com/Phantm7/android_packages_apps_Updater fourteen
+git cherry-pick 022c468
+cd ../../..
+echo "===== Cherry-pick Ended ====="
+
+# keys
+rm -rf vendor/lineage-priv
+git clone https://github.com/Phantm7/build_keys.git -b lin-keys vendor/lineage-priv
+echo "============="
+echo "Keys copied"
+echo "============="
+
+# Remove overrides
+# Define a list of packages to remove
+echo "===== Remove overrides started ====="
+
+OVER_PACKAGES=("GoogleContacts" "GoogleDialer" "PrebuiltBugle" "dialer")
+for PACKAGEU in "${OVER_PACKAGES[@]}"; do
+find vendor/gms -name 'common-vendor.mk' -exec sed -i "/$PACKAGEU/d" {} \;
+done
+echo "===== Remove overrides Success ====="
+
+# Export
+export BUILD_USERNAME=Phantom
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+riseup mi439 userdebug
 rise b
