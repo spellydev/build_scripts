1c7d924ae60bdfd1e63043b15444d38946fb1178
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-02 10:39:15 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests -b a14-projectblaze .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests_clo -b a14-projectblaze .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
