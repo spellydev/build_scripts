f33a4d944e62024005467620dd65f72d85dd361e
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-03 13:42:30 +0600

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests/
 #repo init rom
 repo init --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
 #Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+git clone https://github.com/MisterZtr/treble_manifest.git .repo/local_manifests  -b 13
 
 #build
 /opt/crave/resync.sh
