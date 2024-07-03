1b5a5960d4c22b22913d90b122ca56e940d06edf
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-03 23:53:30 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b ProjectSakura .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b Project-Sakura .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
