66e4c8a1dce7e9f9058e7c2b69c297d92a3b3124
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-05 16:18:33 +0600

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,12 +1,37 @@
+#!/bin/bash
 rm -rf .repo/local_manifests/
-#repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-#Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
 
-#build
-/opt/crave/resync.sh
-. build/envsetup.sh
-riseup mi439 userdebug 
+#repo init rom
+repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+#Local manifests
+git clone https://github.com/Gtajisan/local_manifests -b A11 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Export
+export BUILD_USERNAME=FARHAN-MUH-TASIM
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export TZ=Asia/Dhaka
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh 
+echo "====== Envsetup Done ======="
+
+
+# Lunch
+lunch nad_Mi439_4_19-user 
+echo "============="
+
+# Make cleaninstall
 make installclean
-rise b
+echo "============="
+
+# Build rom
+mka nad
