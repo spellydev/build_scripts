5fe61bb07a5ed673749c371e0dc673e07ca470bf
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-05 08:10:33 +0600

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,12 +1,48 @@
-rm -rf .repo/local_manifests/
-#repo init rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-#Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+#!/bin/bash
 
-#build
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/Evolution-XYZ/manifest -b udc --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/PhantomEnigma/local_manifests -b a14-evoxyz .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# build
 /opt/crave/resync.sh
-. build/envsetup.sh
-riseup mi439 userdebug 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# keys
+mkdir vendor/lineage-priv
+cp build-keys/* vendor/lineage-priv
+echo "============="
+echo "Keys copied"
+echo "============="
+
+# Export
+export BUILD_USERNAME=FARHAN-MUH-TASIM
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch lineage_Mi439_4_19-userdebug || lunch lineage_Mi439_4_19-ap1a-userdebug
+echo "============="
+
+# Make cleaninstall
 make installclean
-rise b
+echo "============="
+
+# Build rom
+m evolution
