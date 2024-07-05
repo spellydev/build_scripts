e9278ccac9bc8c3314a2cf1549f366201801acb5
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-05 08:45:59 +0600

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests -b a14-evoxyz .repo/local_manifests
+git clone https://github.com/Gtajisam/local_manifests -b a14-evoxyz .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
