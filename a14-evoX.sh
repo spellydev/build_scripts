78f054e8f4f643d0b8cec8c84cf6f705948391e6
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-06 12:30:02 +0600

    Update a14-evoX.sh

--- a14-evoX.sh
+++ a14-evoX.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b a14-evoX .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b evoX-14 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
