3497ad3e4973321e5fb9c603f08172b4089f9667
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-06 12:17:56 +0600

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisam/local_manifests -b a14-evoxyz .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b a14-evoxyz .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -21,17 +21,15 @@ echo "Sync success"
 echo "============="
 
 # keys
-mkdir vendor/lineage-priv
-cp build-keys/* vendor/lineage-priv
-echo "============="
-echo "Keys copied"
-echo "============="
+# mkdir vendor/lineage-priv
+# cp build-keys/* vendor/lineage-priv
+# echo "============="
+# echo "Keys copied"
+# echo "============="
 
 # Export
-export BUILD_USERNAME=FARHAN-MUH-TASIM
+export BUILD_USERNAME=FARHAN
 export BUILD_HOSTNAME=crave
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export TZ=Asia/Dhaka
 echo "======= Export Done ======"
 
 # Set up build environment
@@ -40,6 +38,10 @@ echo "====== Envsetup Done ======="
 
 # Lunch
 lunch lineage_Mi439_4_19-userdebug || lunch lineage_Mi439_4_19-ap1a-userdebug
+echo "============="
+
+# Make cleaninstall
+make installclean
 echo "============="
 
 # Build rom
