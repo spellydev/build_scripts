bf4c0f0590ad9f1db31f727cc9bc643163ca6fc8
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-07 23:30:14 +0600

    Update a14-evoX.sh

--- a14-evoX.sh
+++ a14-evoX.sh
@@ -36,7 +36,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_Mi439_4_19-userdebug || lunch lineage_Mi439_4_19-ap1a-userdebug
+lunch lunch lineage_Mi439-user || lunch lineage_Mi439_4_19-user
 echo "============="
 
 # Make cleaninstall
