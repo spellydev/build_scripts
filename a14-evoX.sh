38a5ac945fbd77d1e5198e2b023f861c02eee9a1
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-07 17:59:21 +0600

    Update a14-evoX.sh

--- a14-evoX.sh
+++ a14-evoX.sh
@@ -27,12 +27,11 @@ git cherry-pick f0052a5
 cd ../../..
 
 # keys
-croot && git clone https://github.com/Evolution-XYZ/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
-cd vendor/evolution-priv/keys
-./generate.sh
-echo "============="
-echo "Keys copied"
-echo "============="
+# mkdir vendor/lineage-priv
+# cp build-keys/* vendor/lineage-priv
+# echo "============="
+# echo "Keys copied"
+# echo "============="
 
 # Export
 export BUILD_USERNAME=FARHAN
@@ -44,7 +43,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_Mi439_4_19-user || lunch lineage_Mi439_4_19-userdebug 
+lunch lineage_Mi439_4_19-userdebug || lunch lineage_Mi439_4_19-ap1a-userdebug
 echo "============="
 
 # Make cleaninstall
