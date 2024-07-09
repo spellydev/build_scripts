6ed044266c076a3b17b5dea00fdd62413d731da5
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-09 07:03:43 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,6 +20,13 @@ echo "============="
 echo "Sync success"
 echo "============="
 
+# Custom KeyGen
+curl -O https://raw.githubusercontent.com/ofcsayan/Key-Gen-signed-script/main/generate_all_keys.sh
+chmod +x generate_all_keys.sh
+./generate_all_keys.sh
+echo "============="
+echo "Keys generate"
+echo "============="
 
 # Export
 export BUILD_USERNAME=FARHAN
