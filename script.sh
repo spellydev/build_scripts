45b6a254b8a7e55226f3bcb7e050395f8be3fe02
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-09 06:33:16 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,41 @@
+#!/bin/bash
+
 rm -rf .repo/local_manifests/
-#repo init rom
+
+# repo init rom
 repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
-#Local manifests
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
 git clone https://github.com/PhantomEnigma/local_manifests -b a14-matx .repo/local_manifests
-#build
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# build
 /opt/crave/resync.sh
-. build/envsetup.sh
-lunch lineage_Mi439-ap1a-userdebug
+echo "============="
+echo "Sync success"
+echo "============="
+
+
+# Export
+export BUILD_USERNAME=FARHAN
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch blaze_Mi439_4_19-ap1a-userdebug || lunch blaze_Mi439_4_19-userdebug
+echo "============="
+# Make cleaninstall
 make installclean
-brunch Mi439_4_19 || brunch lineage_Mi439_4_19-ap1a-userdebug || brunch lineage_Mi439_4_19-userdebug
+echo "============="
+
+# Build rom
+mka bacon
