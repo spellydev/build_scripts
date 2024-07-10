4f0575e940508aa6ae90777edf80151ff136b71f
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-10 23:41:28 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,41 @@
-rm -rf .repo/local_manifests/
-#repo init rom
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
-#Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests_clo -b udc-2 .repo/local_manifests
+#!/bin/bash
 
-#build
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/Gtajisan/local_manifests -b a14-matx .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# build
 /opt/crave/resync.sh
-. build/envsetup.sh
-brunch mi439 || brunch lineage_mi439-ap1a-userdebug
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=FARHAN
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch lineage_Mi439_4_19-ap2a-userdebug || lunch lineage_Mi439_4_19-userdebug
+echo "============="
+# Make cleaninstall
+make installclean
+echo "============="
+
+# Build rom
+mka bacon
