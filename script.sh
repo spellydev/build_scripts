3c42873a86a82738fecc34172b37c66beb28bdae
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-10 08:29:12 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -31,7 +31,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_Mi439_4_19-ap1a-userdebug || lunch lineage_Mi439_4_19-userdebug
+lunch lineage_Mi439_4_19-ap2a-userdebug || lunch lineage_Mi439_4_19-userdebug
 echo "============="
 # Make cleaninstall
 make installclean
