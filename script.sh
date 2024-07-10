b69c0d4a5eb6f72bdf4b1b759834e32284fa766f
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-10 23:42:00 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,6 @@
 
 rm -rf .repo/local_manifests/
 
-# repo init rom
-repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
 
 # Local manifests
 git clone https://github.com/Gtajisan/local_manifests -b a14-matx .repo/local_manifests
@@ -14,11 +9,6 @@ echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-# build
-/opt/crave/resync.sh
-echo "============="
-echo "Sync success"
-echo "============="
 
 # Export
 export BUILD_USERNAME=FARHAN
