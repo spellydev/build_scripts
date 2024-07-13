be78c2fe2acf97fb6739011e11935b0dc55c419e
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-13 13:58:33 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b a14-matx .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b Matrixx-14 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
