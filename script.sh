44f45fd1f90ac5e291c29abae117a7ca6d289eec
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-28 13:16:30 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,46 +2,38 @@
 
 rm -rf .repo/local_manifests/
 
-# repo init rom
 repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs
-
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b Alpha-droid .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b Alpha-dr-14 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-# build
-/opt/crave/resync.sh 
+# Sync
+/opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
 
-# keys
-git clone https://github.com/PhantomEnigma/build_keys.git -b blaze-keys vendor/extra
-echo "============="
-echo "Keys copied"
-echo "============="
-
 # Export
-export BUILD_USERNAME=Gtajisan
+export BUILD_USERNAME=FARHAN•2008
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
+#Cherry-pick
+cd vendor/addons
+git fetch 14.0 --unshallow
+git fetch https://github.com/RisingTechOSS/android_vendor_addons.git fourteen
+git cherry-pick dbd659e
+cd ../..
 # Set up build environment
-source build/envsetup.sh || . build/envsetup.sh
+source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
-# Lunch
-lunch lineage_Mi439_4_19-userdebug || lunch lineage_Mi439_4_19-userdebug
-echo "============="
-# Make cleaninstall
+lunch lineage_Mi439_4_19-ap2a-userdebug
 make installclean
-echo "============="
-
-# Build rom
 mka bacon
