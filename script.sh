9213404b347e2b317a5d8cf46f169bc34a7647df
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-29 11:07:08 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14-QPR3
+repo init --depth=1 -u https://github.com/DroidX-UI/manifest.git -b 14_v3 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b a14-projectblaze .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b Droidx-ui .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
