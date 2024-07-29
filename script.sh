adc08bdbf5547bc4c9636335bc21cb27ee9319d9
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-29 15:32:40 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b alpha-droid-14 .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b Alpha-dr-14 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
