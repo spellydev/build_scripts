eed7c6afba675dcd54f87daa095631bfae8090fc
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-07-30 21:11:39 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,36 +1,39 @@
-#ProjectSakura build script
 #!/bin/bash
 
-# Remove existing local_manifests
-echo "Removing existing local_manifests..."
-rm -rf .repo/local_manifests/ || { echo "Failed to remove local_manifests"; exit 1; }
+rm -rf .repo/local_manifests/
 
-# Initialize ProjectBlaze repo
-echo "Initializing ProjectBlaze repo..."
-repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs || { echo "Repo init failed"; exit 1; }
+repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
-# Clone local_manifests repository
-echo "Cloning local_manifests repository..."
-git clone https://github.com/Gtajisam/local_manifests -b Project-Sakura .repo/local_manifests || { echo "Cloning local_manifests failed"; exit 1; }
+# Local manifests
+git clone https://github.com/Gtajisan/local_manifests -b Project-Sakura .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
-# Sync the repositories
-echo "Syncing the repositories..."
-/opt/crave/resync.sh || { echo "Resync failed"; exit 1; }
+# Sync
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
 
-# Set build details
-export BUILD_USERNAME=FARHAN-MUH-TASIM
+# Export
+export BUILD_USERNAME=FARHAN
 export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
 
+#Cherry-pick
+cd vendor/addons
+git fetch 14.0 --unshallow
+git fetch https://github.com/RisingTechOSS/android_vendor_addons.git fourteen
+git cherry-pick dbd659e
+cd ../..
 # Set up build environment
-echo "Setting up build environment..."
-. build/envsetup.sh || { echo "Envsetup failed"; exit 1; }
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
 
-# Lunch configuration
-echo "Configuring lunch..."
-lunch lineage_Mi439_4_19-ap2a-userdebug || { echo "Lunch configuration failed"; exit 1; }
-
-# Build the ProjectBlaze
-echo "Building ProjectBlaze..."
-make bacon || { echo "Build failed"; exit 1; }
-
-echo "Build completed successfully!"
+lunch lineage_Mi439_4_19-ap2a-userdebug
+make installclean
+mka bacon
