7760ed19dc3ee86dc22fb7b73ba40674383e4650
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-03 22:42:22 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -26,6 +26,13 @@ export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 echo "======= Export Done ======"
 
+#Cherry-pick
+cd vendor/addons
+git fetch 14.0 --unshallow
+git fetch https://github.com/RisingTechOSS/android_vendor_addons.git fourteen
+git cherry-pick dbd659e
+cd ../..
+
 # Set up build environment
 . build/envsetup.sh
 echo "====== Envsetup Done ======="
