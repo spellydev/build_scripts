7baa9d8c73ca29f65680f08abecc593e9797fff8
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-05 14:37:12 +0600

    @shitdroid

--- script.sh
+++ script.sh
@@ -14,43 +14,22 @@ echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-cd .repo/repo
-git pull -r
-cd ../..
-
 # Sync the repositories  
 # /opt/crave/resync.sh 
 /opt/crave/resynctest.sh
  echo "====== resynctest Done ======="
 
-# Set up build environment
-export BUILD_USERNAME=FARHAN 
-export BUILD_HOSTNAME=crave 
+# Export
+export BUILD_USERNAME=FARHAN_SENSI
+export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
-
-# Signing (credits sale)
-curl -O https://raw.githubusercontent.com/Gtajisan/crDroid-build-signed-script/crdroid/create-signed-env.sh
-chmod +x create-signed-env.sh
-./create-signed-env.sh
-echo "====== signing Done ======="
 
 #Cherry-pick
 cd vendor/addons
-git fetch 14.0 --unshallow
-git fetch https://github.com/AfterlifeOS/vendor_afterlife.git 14
+git fetch crdroid --unshallow
+git fetch https://github.com/RisingTechOSS/android_vendor_addons fourteen
 git cherry-pick dbd659e
 cd ../..
-
-# Remove overrides
-# Define a list of packages to remove
-echo "===== Remove overrides started ====="
-
-OVER_PACKAGES=("GoogleContacts" "GoogleDialer" "PrebuiltBugle" "dialer")
-for PACKAGEU in "${OVER_PACKAGES[@]}"; do
-find vendor/gms -name 'common-vendor.mk' -exec sed -i "/$PACKAGEU/d" {} \;
-done
-echo "===== Remove overrides Success ====="
-
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
