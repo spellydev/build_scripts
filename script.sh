523fe6912d9b345502e3fc906ecd5660942ffb5f
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-10 10:15:08 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -18,22 +18,25 @@ echo "============================"
 /opt/crave/resync.sh
 echo "============================"
 
+#  remove to fix build error 
+rm -rf external/chromium-webview  
+echo "======= remove Done ======"
+
+# remove package 
+rm -rf packages/apps/ParanoidSense
+echo "======= remove Done ======"
+
+
 # Export
 export BUILD_USERNAME=FARHAN
 export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 echo "======= Export Done ======"
 
 
 # Set up build environment
-# source build/envsetup.sh
+. build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-. build/envsetup.sh
-goafterlife Mi439_4_19 userdebug
-
-
-
-
-
-
+goafterlife Mi439_4_19 userdebug"
