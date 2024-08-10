58777ee71b868a809d7c0e16625cbf158728510a
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-10 10:45:59 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -31,6 +31,7 @@ echo "======= remove Done ======"
 export BUILD_USERNAME=FARHAN
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 echo "======= Export Done ======"
 
 
