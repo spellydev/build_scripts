2c0c70ad826615f2465d2bdcef5c633d54b1dbbe
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-16 16:04:57 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14
+repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
