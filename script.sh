7f425110549f074e1da123ebb0402f81982f1b4b
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-21 14:59:19 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ echo "gay DerpFest Repo init success"
 echo "=============================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b 14/derp .repo/local_manifests --git-lfs
+git clone https://github.com/Gtajisan/local_manifests -b 14/derp .repo/local_manifests
 echo "========================================="
 echo "Fuckin# shit Local manifest clone success"
 echo "========================================="
@@ -22,18 +22,18 @@ echo "Sync successfully Done"
 echo "======================"
 
 # Export
-export BUILD_USERNAME=FARHAN_shit
+export BUILD_USERNAME=derpshit
 export BUILD_HOSTNAME=crave
 echo "============="
 
 # Set up build environment
 source build/envsetup.sh
 echo "======================================"
-echo "im crave Farhan sir you rom sync Done"
+echo "im crave sir you rom sync Done ok bye "
 echo "======================================"
 
 # Lunch
-lunch derp_mi439-ap2a-userdebug 
+lunch derp_Mi439_4_19-ap2a-userdebug 
 echo "============="
 
 # Make cleaninstall
