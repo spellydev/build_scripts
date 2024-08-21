17ad4f5f95de4fc70c7a6cdf61272d776056120c
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-21 14:57:09 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,13 +4,13 @@ rm -rf .repo/local_manifests/
 
 
 # repo init rom
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs
 echo "=============================="
 echo "gay DerpFest Repo init success"
 echo "=============================="
 
 # Local manifests
-git clone https://github.com/Gtajisan/local_manifests -b 14/derp .repo/local_manifests
+git clone https://github.com/Gtajisan/local_manifests -b 14/derp .repo/local_manifests --git-lfs
 echo "========================================="
 echo "Fuckin# shit Local manifest clone success"
 echo "========================================="
@@ -29,13 +29,11 @@ echo "============="
 # Set up build environment
 source build/envsetup.sh
 echo "======================================"
-echo "im craven Farhan sir you rom sync Done"
+echo "im crave Farhan sir you rom sync Done"
 echo "======================================"
-# Setup Git LFS
-# repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
 
 # Lunch
-lunch derp_mi439-ap1a-userdebug || lunch derp_mi439-userdebug
+lunch derp_mi439-ap2a-userdebug 
 echo "============="
 
 # Make cleaninstall
