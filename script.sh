8f213fd98f65faf7b3e4e7a31d0cf2086f5a7d54
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-24 00:19:26 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,6 +20,14 @@ echo "============="
 echo "Sync success"
 echo "============="
 
+# Git cherry-pick
+cd vendor/lineage
+git remote add los https://github.com/LineageOS/android_vendor_lineage
+git fetch los
+git cherry-pick 198966577ace63573e5be49e03a2e59e32997054
+cd ../..
+echo "===== Cherry-picking Success ====="
+
 # Export
 export BUILD_USERNAME=FARHAN_UN
 export BUILD_HOSTNAME=crave
