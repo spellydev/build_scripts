42af3a1cc5950fe6763b955a39e2382a6417a1ed
Jayzee-Zee <i.am.hipop@gmail.com>
2024-08-25 08:13:11 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
 git config --global user.email salitagwapo@gmail.com
 git config --global user.name Jayzee-Zee
 
