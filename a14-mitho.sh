d66e93fb34551302fbaf38a0171d591015b991ca
Jayzee-Zee <i.am.hipop@gmail.com>
2024-08-25 15:26:41 +0800

    Update a14-mitho.sh

--- a14-mitho.sh
+++ a14-mitho.sh
@@ -1,14 +1,14 @@
 rm -rf .repo/local_manifests/
-repo init rom
+echo "repo init rom"
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-Local manifests
-git clone https://github.com/PhantomEnigma/local_manifests .repo/local_manifests
-build
+#Local manifests
+git clone https://github.com/Jayzee-Zee/local_manifests/tree/Rising .repo/local_manifests
+#build
 /opt/crave/resync.sh
 . build/envsetup.sh
-riseup Mi439_4_19 userdebug 
-make installclean
-cd kernel/xiaomi/mithorium-4.19/kernel
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+riseup earth userdebug 
+#make installclean
+#cd kernel/xiaomi/mithorium-4.19/kernel
+#curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
 croot
 rise b
