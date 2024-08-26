8eff2c491d05bbb56c26198a2fbb3e660e1c5f1b
Jayzee-Zee <i.am.hipop@gmail.com>
2024-08-26 13:29:18 +0800

    Update and rename a14-mitho.sh to build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+rm -rf .repo/local_manifests/  && # Clone local_manifests repository
+repo init -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs --depth=1
+#clone dev tree
+git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b Rising .repo/local_manifests && 
+# Sync the repositories
+/opt/crave/resync.sh  && 
+# Set up build environment
+export BUILD_USERNAME=Jayzee-Zee 
+export BUILD_HOSTNAME=crave
+export TARGET_PRODUCT=lineage_earth
+export TARGET_RELEASE=ap2a
+export RISING_MAINTAINER=Jayzee-Zee 
+source build/envsetup.sh
+ 
+# Build the ROM
+riseup lineage_earth user && make installclean && rise b
