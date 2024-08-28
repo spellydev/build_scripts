3d1b7085bb6543e8dd87b92e339dd03ac29e8097
FrhnDroid <hanzffx@gmail.com>
2024-08-28 23:20:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@ rm -rf out/target/product/earth/*
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
 repo init -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs --depth=1
 #clone dev tree
-git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b Rising .repo/local_manifests &&
+git clone https://github.com/FrhnDroid/Local-Manifest --depth 1 -b RisingGAPPS .repo/local_manifests &&
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
-export BUILD_USERNAME=Jayzee-Zee 
+export BUILD_USERNAME=Farhan
 export BUILD_HOSTNAME=crave
 #export TARGET_PRODUCT=lineage_earth
 #export TARGET_RELEASE=ap2a
 export TZ=Asia/Jakarta
-export RISING_MAINTAINER=Jayzee-Zee 
+export RISING_MAINTAINER=👾FrhnDroid👾 
 source build/envsetup.sh
  
 # Build the ROM
