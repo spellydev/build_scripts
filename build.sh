c9d9aaf832644d830ce608dc20d6b80a0fa5d9c0
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-13 22:54:06 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 rm -rf out/target/product/earth/*
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
-repo init -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs --depth=1
+repo init -u https://github.com/AlphaDroid-Project/manifest.git -b alpha-14 --git-lfs --depth=1
 #clone dev tree
-git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b Rising .repo/local_manifests &&
+git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b Alpha .repo/local_manifests &&
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
@@ -11,11 +11,11 @@ export BUILD_HOSTNAME=crave
 #export TARGET_PRODUCT=lineage_earth
 #export TARGET_RELEASE=ap2a
 export TZ=Asia/Jakarta
-export RISING_MAINTAINER=Jayzee-Zee
 source build/envsetup.sh
  
 # Build the ROM
- riseup earth user
+lunch lineage_earth-userdebug
 
 #beelding
-rise b
+
+make bacon
