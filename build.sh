7940bfc310419e88f75b70e4b05e9a684d134b45
Jayzee-Zee <i.am.hipop@gmail.com>
2024-09-14 15:53:48 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,15 @@ rm -rf hardware/xiaomi
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
 repo init -u https://github.com/AlphaDroid-Project/manifest.git -b alpha-14 --git-lfs --depth=1
 #clone dev tree
-git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b Alpha .repo/local_manifests &&
-# Sync the repositories
+#git clone https://github.com/Jayzee-Zee/Local-Manifest --depth 1 -b Alpha .repo/local_manifests &&
+# Sync the 
+#repositories
+git clone https://github.com/Jayzee-Zee/android_device_xiaomi_earth -b Alpha device/xiaomi/earth 
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth -b lineage-21 vendor/xiaomi/earth
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
+git clone https://github.com/Jayzee-Zee/android_kernel_xiaomi_earth -b nonksu kernel/xiaomi/earth
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21 hardware/mediatek &&
 /opt/crave/resync.sh  && 
 # Set up build environment
 export BUILD_USERNAME=Jayzee-Zee 
