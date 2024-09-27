2488af2594eb2730d8befce8374b7056d1ae6ee5
FrhnDroid <hanzffx@gmail.com>
2024-09-28 00:17:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
 rm -rf out/target/product/earth/*
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
-repo init -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs --depth=1
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs --depth 1
 #clone dev tree
-git clone https://github.com/FrhnDroid/Local-Manifest --depth 1 -b RisingGAPPS .repo/local_manifests &&
+git clone https://github.com/FrhnDroid/Local-Manifest --depth 1 -b Derp-14 .repo/local_manifests &&
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
 export BUILD_USERNAME=Farhan
 export BUILD_HOSTNAME=crave
-#export TARGET_PRODUCT=lineage_earth
+#export TARGET_PRODUCT=derp_earth
 #export TARGET_RELEASE=ap2a
-export TZ=Asia/Ipoh
-export RISING_MAINTAINER=👾FrhnDroid👾 
+export TZ=Asia/Ipoh 
 source build/envsetup.sh
  
 # Build the ROM
-riseup earth user && rise b
+lunch derp_earth-user && mka derp
