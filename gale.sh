387be547c88fba22161c1e019e4569b86b4b0be4
Spelly <michicaputo6@gmail.com>
2024-10-14 17:51:07 +0200

    Update gale.sh

--- gale.sh
+++ gale.sh
@@ -2,16 +2,16 @@ rm -rf out/target/product/gale/*
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
 repo init -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs --depth=1
 #clone dev tree
-git clone https://github.com/FrhnDroid/Local-Manifest --depth 1 -b RisingGALE .repo/local_manifests &&
+git clone https://github.com/spellydev/Local-Manifest --depth 1 -b RisingGALE .repo/local_manifests &&
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
-export BUILD_USERNAME=Farhan
+export BUILD_USERNAME=spellydev
 export BUILD_HOSTNAME=crave
 #export TARGET_PRODUCT=lineage_gale
 #export TARGET_RELEASE=ap2a
 export TZ=Asia/Ipoh
-export RISING_MAINTAINER=👾FrhnDroid👾 
+export RISING_MAINTAINER=itzSpelly
 source build/envsetup.sh
  
 # Build the ROM
